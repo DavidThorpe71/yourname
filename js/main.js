f927f9005903fd891c540e431b2654604efdb22a
David Thorpe <davidthorpe878@gmail.com>
2017-08-22 19:24:48 +0100

    javascript update to seperate words

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var words = 'your name'
+var words = ['your', 'name'];
 
 var wordsLength = words.length
 
@@ -7,9 +7,12 @@ var container = document.getElementById('container');
 var div = document.createElement('div');
 
 for (var i = 0; i < wordsLength; i++) {
-	var letter = words[i];
-	var div = document.createElement('div');
-	div.className = 'box box' + i;
-	div.innerHTML = letter.toUpperCase();
-	container.appendChild(div)
+	var word = words[i];
+	for (var j = 0; j < word.length; j++) {
+		var div = document.createElement('div');
+		div.className = 'row' + i + ' box' + j + ' box';
+		var letter = word[j];
+		div.innerHTML = letter.toUpperCase();
+		container.appendChild(div)
+	}	
 }
